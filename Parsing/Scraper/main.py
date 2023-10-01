711708ed490b9f7b44d2aa79604de98a86208aec
AB-GAMES\andrey.vlasiuk <andrey.vlasiuk@ab-games.com>
2023-10-01 14:17:37 +0300

    Переписав Main функці.

--- Parsing/Scraper/main.py
+++ Parsing/Scraper/main.py
@@ -3,26 +3,15 @@ from quotes_scraper.spiders.quotes_spider import QuotesSpider
 from quotes_scraper.spiders.authors_spider import AuthorsSpider
 
 
-# Запуск краулера для цитат
-def run_quotes_crawler():
-    process = CrawlerProcess(settings={
-        'FEED_FORMAT': 'json',
-        'FEED_URI': 'quotes.json'
-    })
-    process.crawl(QuotesSpider)
-    process.start()
-
-
-# Запуск краулера для авторів
-def run_authors_crawler():
+def run_crawlers():
     process = CrawlerProcess(settings={
         'FEED_FORMAT': 'json',
         'FEED_URI': 'authors.json'
     })
+    process.crawl(QuotesSpider)
     process.crawl(AuthorsSpider)
     process.start()
 
 
 if __name__ == '__main__':
-    run_quotes_crawler()
-    run_authors_crawler()
+    run_crawlers()
--- Parsing/Scraper/quotes_scraper/spiders/authors_spider.py
+++ Parsing/Scraper/quotes_scraper/spiders/authors_spider.py
@@ -17,7 +17,7 @@ class AuthorsSpider(scrapy.Spider):
                 yield scrapy.Request(url=f"http://quotes.toscrape.com/author/{author}/",
                                      callback=self.parse_author)
         next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
+        if next_page:
             yield response.follow(next_page, self.parse)
 
     def parse_author(self, response):
