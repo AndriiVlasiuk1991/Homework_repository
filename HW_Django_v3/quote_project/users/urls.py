aa6a89515d2ded83ee7ded954b75d981a29b568c
AB-GAMES\andrey.vlasiuk <andrey.vlasiuk@ab-games.com>
2023-12-03 15:32:18 +0200

    Hw_10_v.3

---
+++ HW_Django_v3/quote_project/users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView, profile
+from .forms import LoginForm
+
+app_name = "users"
+
+urlpatterns = [
+    path("signup/", RegisterView.as_view(), name="signup"),
+    path("login/",
+         LoginView.as_view(template_name="users/login.html", form_class=LoginForm, redirect_authenticated_user=True),
+         name="login"),
+    path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path("profile/", profile, name="profile"),
+]
